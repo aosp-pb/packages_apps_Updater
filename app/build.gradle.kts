0048c0905f298bf712a3f230fcd679e72d905cf8
Abhinav Kumar <122148266+acex88@users.noreply.github.com>
2024-03-01 18:42:28 +0530

    Updater: Change package name to avoid conflict
    Change-Id: I0724b6b5a3ab27f880914750182c6ee6a602174f

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
-import org.lineageos.generatebp.GenerateBpPlugin
-import org.lineageos.generatebp.GenerateBpPluginExtension
-import org.lineageos.generatebp.models.Module
+import com.aospb.generatebp.GenerateBpPlugin
+import com.aospb.generatebp.GenerateBpPluginExtension
+import com.aospb.generatebp.models.Module
 
 plugins {
     id("com.android.application")
@@ -18,7 +18,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.lineageos:gradle-generatebp:+")
+        classpath("com.aospb:gradle-generatebp:+")
     }
 }
 
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "com.aospb.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
