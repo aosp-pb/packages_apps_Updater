af804746f3a8982a5d48d5eaf9746062f1a8508a
clarencelol <clarencekuiek@proton.me>
2024-03-26 00:23:58 +0800

    Updater: Initial aosPB Project bringup
    Change-Id: I4dd567d7d72e22f7fd64f41a7be837a3ce187001
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 import java.util.Properties
-import org.lineageos.generatebp.GenerateBpPlugin
-import org.lineageos.generatebp.GenerateBpPluginExtension
-import org.lineageos.generatebp.models.Module
+import org.aospb.generatebp.GenerateBpPlugin
+import org.aospb.generatebp.GenerateBpPluginExtension
+import org.aospb.generatebp.models.Module
 
 plugins {
     id("com.android.application")
@@ -18,7 +18,7 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.lineageos:gradle-generatebp:+")
+        classpath("org.aospb:gradle-generatebp:+")
     }
 }
 
@@ -33,7 +33,7 @@ android {
     compileSdk = 33
 
     defaultConfig {
-        applicationId = "org.lineageos.updater"
+        applicationId = "org.aospb.updater"
         minSdk = 30
         targetSdk = 33
         versionCode = 1
