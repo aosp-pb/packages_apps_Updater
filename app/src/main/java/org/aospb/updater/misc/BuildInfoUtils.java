f46edb402a068a2fd7aec4f7ee5018c0cd6682f1
sajidshahriar72543 <sazidshahriar39@gmail.com>
2024-11-13 18:24:16 +0600

    Updater: Move to some of our custom strings
    - Also get rid of the build version dependency

--- app/src/main/java/org/aospb/updater/misc/BuildInfoUtils.java
+++ app/src/main/java/org/aospb/updater/misc/BuildInfoUtils.java
@@ -26,7 +26,11 @@ public final class BuildInfoUtils {
         return SystemProperties.getLong(Constants.PROP_BUILD_DATE, 0);
     }
 
-    public static String getBuildVersion() {
-        return SystemProperties.get(Constants.PROP_BUILD_VERSION);
+    // public static String getBuildVersion() {
+    //     return SystemProperties.get(Constants.PROP_BUILD_VERSION);
+    // }
+
+    public static String getBrand() {
+        return SystemProperties.get(Constants.PROP_BRAND);
     }
 }
